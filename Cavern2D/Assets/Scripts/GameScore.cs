6405cf8064a6b413ecce52453942257a51c93391
miikaran <42628@edu.raseko.fi>
2022-04-04 20:34:30 +0300

    RestartSreen Score +  designing mainmenu

--- Cavern2D/Assets/Scripts/GameScore.cs
+++ Cavern2D/Assets/Scripts/GameScore.cs
@@ -7,9 +7,12 @@ public class GameScore : MonoBehaviour
 {
 
     public Text scoreText;
+    public Text restartScreenScore;
     static private int scoreNumber;
 
-    //Adds score when enemy collides with enemies.
+
+
+    //Adds score when bullet collides with enemies.
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Bullet")
@@ -17,6 +20,15 @@ public class GameScore : MonoBehaviour
         {
             scoreNumber += 10;
             scoreText.text = "Score: " + scoreNumber;
+        }
+
+        //Updates score for restart screen.
+
+        if (other.tag == "Bullet")
+
+        {
+            scoreNumber += 0;
+            restartScreenScore.text = "Your score was: " + scoreNumber;
         }
     }
 }
